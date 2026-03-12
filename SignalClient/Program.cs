8fb005aedc30ab5c5241ed3603681c5c34a8e13f
dlldotusha <mishaduxin@gmail.com>
2026-03-12 16:26:39 +0300

    Добавлены сервер и клиент SignalPong, базовые настройки
    Созданы проекты SigalServer и SignalClient на .NET 10.0. Добавлены стандартные файлы ASP.NET Core для сервера, реализован рендер поля в Program.cs, подключена библиотека SignalR для клиента. Обновлён файл решения для обоих проектов.

---
+++ SignalClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Render(int PlayerX, int PlayerY, int MaxX, int MaxY)
+    {
+        Console.WriteLine("Render");
+        for (int Y = 0; Y < MaxY; Y++)
+        {
+            for (int X = 0; X < MaxX; X++)
+            {
+                if (X == PlayerX && Y == PlayerY)
+                {
+                    Console.Write(" □");
+                }
+                else
+                {
+                    Console.Write(" ■");
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+
+    public static void Main()
+    {
+        Render(1, 1, 5, 5);
+    }
+}
